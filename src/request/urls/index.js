70fd21c4c32420eeb9f1409815350bc28f87d5ff
Sky Yao <challenge.cool@gmail.com>
2022-11-23 09:23:43 +0800

    fix: remove unused import

--- src/request/urls/index.js
+++ src/request/urls/index.js
@@ -1,21 +1,5 @@
-import group from './modules/group'
-import settings from './modules/settings'
-import iwork from './modules/iwork'
-import iteration from './modules/iteration'
-import project from './modules/project'
-import ici from './modules/ici'
-import ione from './modules/ione'
-import user from './modules/user'
-import thirdParty from './modules/third-party'
+import user from './user'
 
 export default {
-  group,
-  settings,
-  iwork,
-  iteration,
-  project,
-  ici,
-  ione,
-  user,
-  thirdParty
+  user
 }
